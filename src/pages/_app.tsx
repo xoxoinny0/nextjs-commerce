ccb50717ce8c2d3e7d4f060cea30aece2183e66f
xoxoinny0 <xoxoinny0@gmail.com>
2023-06-10 16:49:09 +0900

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,6 @@
 import 'src/styles/globals.css'
 import type { AppProps } from 'next/app'
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { SessionProvider } from 'next-auth/react'
 import Header from '@components/Header'
 
